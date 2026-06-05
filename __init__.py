e168be129f04a489186b8b76aefa782726b81de9
Akshay R <157674473+Akshay-Ramesh-VITC@users.noreply.github.com>
2026-06-05 20:19:57 +0530

    Bump version from 0.1.2 to 0.2.3

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ from .backend import get_backend, _BACKEND
 from .backend_manager import SmartSummary, TrainingMonitor, ModelView, draw_graph, get_component
 from .backend_manager import toONNX, fromONNX
 
-__version__ = "0.1.2"
+__version__ = "0.2.3"
 __author__ = "Akshay"
 __all__ = ["get_backend", "TrainingMonitor", "SmartSummary", "ModelView", "draw_graph", "get_component", "toONNX", "fromONNX"]
 
